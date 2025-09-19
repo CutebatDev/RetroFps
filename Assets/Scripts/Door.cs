d27e0f1a532a33c5e03a099ecfeabc04fbf73875
S3raphimm <167085675+S3raphimm@users.noreply.github.com>
2025-09-20 01:48:54 +0300

    Doors ! & Day Animation

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -8,10 +8,12 @@ public class Door : MonoBehaviour
     
     [SerializeField] private GameObject doorObject;
     
-    public bool isAnimating = false;
-    [SerializeField] private float openSpeed = 2.0f;
-    [SerializeField] private float zDegreesClosed = .0f;
-    [SerializeField] private float angleDegreesToConsiderClosed = 2.0f;
+    [HideInInspector] public bool isAnimating = false; // Public in case we want to block player when opening
+    public bool openInOtherDirection = false;
+    [SerializeField] private float rotationSpeedDegreesPerSecond = 45.0f;
+    [SerializeField] private float angleDegreesToConsiderClosed = .1f;
+    private Quaternion targetRotation;
+    public Vector3 axis = Vector3.up;
     
     public void TryOpen()
     {
@@ -25,19 +27,32 @@ public class Door : MonoBehaviour
             // PLEASE LET ME KNOW IF THIS WORKS 
             // DONT WANT TO DESTROY THE OBJECT SO IT'S STILL VISIBLE
             gameObject.tag = "Untagged"; // So player movement doesn't detect it anymore
-            isAnimating = true;
+            StartRotation();
             //Destroy(gameObject);
         }
     }
 
+    private void StartRotation()
+    {
+        float degrees = openInOtherDirection ? 90f : -90f;
+        targetRotation = doorObject.transform.rotation * Quaternion.AngleAxis(degrees, axis);
+        isAnimating = true;
+    }
+    
 
     void Update()
     {
         if (!isAnimating)
             return;
         
-        transform.Rotate(Vector3.up, openSpeed * Time.deltaTime);
-        if (Math.Abs(transform.rotation.eulerAngles.z - zDegreesClosed) <= angleDegreesToConsiderClosed) {
+        doorObject.transform.rotation = Quaternion.RotateTowards(
+            doorObject.transform.rotation,
+            targetRotation,
+            rotationSpeedDegreesPerSecond * Time.deltaTime
+        );
+        
+        if (Quaternion.Angle(doorObject.transform.rotation, targetRotation) <= angleDegreesToConsiderClosed) {
+            doorObject.transform.rotation = targetRotation; 
             isAnimating = false;
         }
     }
